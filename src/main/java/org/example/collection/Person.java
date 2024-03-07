5edff16aa0424d4cf9675a4c4a08d37586d37d25
PolinaKaluga <polina_kalugina_2000@mail.ru>
2024-03-07 21:45:03 +0300

    base

--- src/main/java/org/example/collection/Person.java
+++ src/main/java/org/example/collection/Person.java
@@ -1,5 +1,7 @@
 package org.example.collection;
 
+import org.example.expection.InvalidValue;
+
 public class Person {
     private Long height; //Поле может быть null, Значение поля должно быть больше 0
 
@@ -14,11 +16,15 @@ public class Person {
         this.eyeColor = eyeColor;
         this.hairColor = hairColor;
     }
+
+
     public Float getWeight() {
         return weight;
     }
 
-    public void setWeight(Float weight) {
+    public void setWeight(Float weight) throws InvalidValue {
+        if(weight == null){throw new InvalidValue("Вес не может быть пустым");}
+        if(weight <= 0){throw new InvalidValue("Вес должен быть больше нуля");}
         this.weight = weight;
     }
 
@@ -26,7 +32,9 @@ public class Person {
         return height;
     }
 
-    public void setHeight(Long height) {
+    public void setHeight(Long height) throws InvalidValue{
+        if(height == null){throw new InvalidValue("Рост не может быть пустым");}
+        if(height <= 0){throw new InvalidValue("Рост должен быть больше нуля");}
         this.height = height;
     }
 
@@ -34,7 +42,9 @@ public class Person {
         return eyeColor;
     }
 
-    public void setEyeColor(EyeColor eyeColor) {
+    public void setEyeColor(EyeColor eyeColor) throws InvalidValue {
+        if(eyeColor == null){throw new InvalidValue("Цвет глаз не может быть пустым");}
+
         this.eyeColor = eyeColor;
     }
 
@@ -43,7 +53,8 @@ public class Person {
         return hairColor;
     }
 
-    public void setHairColor(HairColor hairColor) {
+    public void setHairColor(HairColor hairColor)throws InvalidValue  {
+        if(hairColor == null){throw new InvalidValue("Цвет волос не может быть пустым");}
         this.hairColor = hairColor;
     }
 }
